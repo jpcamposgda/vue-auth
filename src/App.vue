692a24ed0e2f0a28a4451339370f3089761864bc
João pedro da silva campos <jpcamposgda@gmail.com>
2022-04-20 15:02:06 -0400

    logout

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,18 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-bytebank mb-5">
-      <a class="navbar-brand" href="#">Connect</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <img src="../img/options.svg" alt="options">
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <router-link class="nav-link" to="/">Home</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/gerentes" class="nav-link">
-              Gerentes
-            </router-link>
-
-          </li>
-          
-          <li class="nav-item">
-            <router-link to="/Login" class="nav-link">
-              Login
-            </router-link>
-
-          </li>
-          
-          
-          <li class="nav-item">
-
-            <a 
-            href="#"
-            class="nav-link"
-            @click.prevent="efetuarLogout">Logout</a>
-
-          </li>
-            
-        </ul>
-      </div>
-    </nav>
+    <BarraNavegacao />
     <router-view/>
   </div>
 </template>
 
 <script>
-export default {
-  
-methods:{
-  efetuarLogout () {
-    localStorage.removeItem('token') 
-    this.$router.push({name: 'login'})
-  }
+
+import BarraNavegacao from '@/components/BarraNavegacao'
+
+export default{
+
+components: {
+  BarraNavegacao
 }
 
 }
@@ -57,16 +21,3 @@ methods:{
 
 
 
-<style>
-.navbar {
-  background: #140083;
-}
-.navbar-bytebank a {
-  color: #fff;
-}
-.navbar-bytebank a:hover {
-  color: #000;
-}
-
-
-</style>
---
+++ src/components/BarraNavegacao.vue
@@ -0,0 +1,54 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-bytebank mb-5">
+      <a class="navbar-brand" href="#">Connect</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <img src="./../../img/options.svg" alt="options">
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+       <BarraNavIn v-if="usuarioestaLogado"/>
+       <BarraNavOut v-else/>
+
+       </div>
+    </nav>
+</template>
+
+
+<script>
+
+import BarraNavIn from './BarraNavIn';
+import BarraNavOut from './BarraNavOut';
+
+export default {
+
+  components:{
+    BarraNavIn,
+    BarraNavOut,
+   
+
+},
+
+
+computed: {
+  usuarioestaLogado() {
+
+return Boolean(localStorage.getItem('token'))
+
+    }
+   }
+  }
+
+</script>
+
+
+<style>
+.navbar {
+  background: #140083;
+}
+.navbar-bytebank a {
+  color: #fff;
+}
+.navbar-bytebank a:hover {
+  color: #000;
+}
+
+</style>
